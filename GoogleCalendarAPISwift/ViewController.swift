291b5161666b19797d5b5eeea435c5fb78251162
Eduardo Hoyos <edu.hoyos92@gmail.com>
2017-06-29 02:54:41 -0500

    Initial Commit

---
+++ GoogleCalendarAPISwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleCalendarAPISwift
+//
+//  Created by Eduardo Hoyos on 6/29/17.
+//  Copyright © 2017 Jcodee SAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
